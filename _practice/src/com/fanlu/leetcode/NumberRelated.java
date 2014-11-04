5a74f6bf444d289227d226651cfcde2254663c49
Fanlu91 <fanlu91@gmail.com>
2014-11-04 02:01:36 +0000

    Valid Palindrome Pascal's Triangle II

--- _practice/src/com/fanlu/leetcode/NumberRelated.java
+++ _practice/src/com/fanlu/leetcode/NumberRelated.java
@@ -22,11 +22,59 @@ public class NumberRelated {
 		}
 		return result;
 	}
-
+    /**
+     * check whether an integer is palindrome
+     * @param x
+     * @return
+     */
+	public boolean isPalindrome(int x) {
+        char[] chars=Integer.toString(x).toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if(chars[i]!=chars[chars.length-1-i]){
+				return false;
+			}
+		}
+		return true;
+    }
 	/**
-	 * generate Pascal's Triangle.
-	 * using factorial tool and the formula(j and row number n;) of index. 
-	 * no a very good answer.
+     * check whether an string input is palindrome
+     * @param x
+     * @return
+     */
+	public boolean isPalindrome(String s) {
+        if(null==s)
+            return true;
+        char[] letters= s.trim().toLowerCase().toCharArray();
+        int l=0;
+        int r= letters.length-1;
+        while(l<r){
+        	if(isValidLetter(letters[l])){
+        		if(isValidLetter(letters[r])){
+        			if(letters[l]!=letters[r])
+        				return false;
+        			else{
+        				l++;
+        				r--;
+        			}        				
+        		}else{
+        			r--;
+        		}
+        	}else{
+        		l++;
+        	}
+        }        
+        return true;
+    }
+	public boolean isValidLetter(char c){
+		if((c>='a'&&c<='z')||(c>='0'&&c<='9'))
+			return true;
+		else
+			return false;
+	}
+	/**
+	 * generate Pascal's Triangle. using factorial tool and the formula(j and
+	 * row number n;) of index. no a very good answer.
+	 * 
 	 * @param numRows
 	 * @return
 	 */
@@ -35,48 +83,79 @@ public class NumberRelated {
 		for (int i = 1; i <= numRows; i++) {
 			List<Integer> theList = new ArrayList<Integer>();
 			for (int j = 1; j <= i; j++) {
-				//the formula
-				BigInteger bi= (factorial(i - 1).divide(factorial(i - j)).divide(factorial(j - 1)));
+				// the formula
+				BigInteger bi = (factorial(i - 1).divide(factorial(i - j))
+						.divide(factorial(j - 1)));
 				theList.add(bi.intValue());
 			}
 			list.add(theList);
 		}
 		return list;
 	}
+
 	/**
-	 * use iteration to get the result from the previous list.
-	 * ac solution
+	 * use iteration to get the result from the previous list. ac solution
+	 * 
 	 * @param numRows
 	 * @return
 	 */
 	public List<List<Integer>> pascalTriangle(int numRows) {
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
-		if(numRows==0){
+		if (numRows == 0) {
 			return list;
 		}
 		List<Integer> element = new ArrayList<Integer>();
 		element.add(1);
-    	for(int i=0;i<numRows;i++){
-    		list.add(element);
-    		element=this.getNextRow(element);
-    	}
+		for (int i = 0; i < numRows; i++) {
+			list.add(element);
+			element = this.getNextRow(element);
+		}
 		return list;
 	}
-	public List<Integer> getNextRow(List<Integer> previous){
+
+	public List<Integer> getNextRow(List<Integer> previous) {
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(1);
-		for (int i = 0; i < previous.size()-1; i++) {
-			list.add(previous.get(i)+previous.get(i+1));
+		for (int i = 0; i < previous.size() - 1; i++) {
+			list.add(previous.get(i) + previous.get(i + 1));
 		}
 		list.add(1);
 		return list;
 	}
+
+	/**
+	 * Given an index k, return the kth row of the Pascal's triangle. use only
+	 * O(k) extra space
+	 * 
+	 */
+	public List<Integer> getRow(int rowIndex) {
+		List<Integer> list = new ArrayList<Integer>();
+		if (rowIndex < 0)
+			return list;
+		list.add(1);
+		for (int j = 1; j <= rowIndex; j++) {
+			//j represents the index in the list;
+			//for each index we need to do the addition using the loop below.
+			int left = list.get(0);
+			for (int i = 1; i < j; i++) {
+				int right = list.get(i);
+				list.set(i, left+right);
+				left= right;
+//				System.out.println(Arrays.toString(list.toArray()));
+
+			}
+			list.add(1);
+		}
+		return list;
+	}
+
 	/**
 	 * factorial(if long is used, 21 will cause overflow)
+	 * 
 	 * @param n
 	 * @return
 	 */
-	private BigInteger factorial(int n) {	
+	private BigInteger factorial(int n) {
 		if (n < 0)
 			return new BigInteger(String.valueOf(0));
 		if (n == 0)
@@ -86,11 +165,13 @@ public class NumberRelated {
 	}
 
 	/**
-	 * factorial, using int array to store the result(reverse order)
-	 * according to http://www.cnblogs.com/anderslly/archive/2008/05/19/factorial-algorithms.html
-	 * but there is a problem in his code(should fill the array with 1 first)
-	 * something more about factorial: how many 0 at the end of 100! ? (hint: Prime number. 2a¡Á5b¡Áp1a1...pnan, 
-	 * The number of 0 depends on how many 5 those numbers contains, answer is 24=20+4[25 50 75 100]) 
+	 * factorial, using int array to store the result(reverse order) according
+	 * to http://www.cnblogs.com/anderslly/archive/2008/05/19/factorial-
+	 * algorithms.html but there is a problem in his code(should fill the array
+	 * with 1 first) something more about factorial: how many 0 at the end of
+	 * 100! ? (hint: Prime number. 2a¡Á5b¡Áp1a1...pnan, The number of 0 depends on
+	 * how many 5 those numbers contains, answer is 24=20+4[25 50 75 100])
+	 * 
 	 * @param n
 	 * @return
 	 */
@@ -109,31 +190,32 @@ public class NumberRelated {
 		long carry = 0;
 		Arrays.fill(array, 1);
 		for (int i = 2; i <= n; i++) {
-			//do the Multiplication; 
-			//Multiplier will be the new 'n' and each digital of the result before
+			// do the Multiplication;
+			// Multiplier will be the new 'n' and each digital of the result
+			// before
 			for (int j = 0; j < digits; j++) {
-//				System.out.println("carry value"+carry);			
+				// System.out.println("carry value"+carry);
 				long multipleResult = array[j] * i + carry;
-//				System.out.println(multipleResult);
+				// System.out.println(multipleResult);
 				array[j] = (int) (multipleResult % 10);
 				carry = multipleResult / 10;
 			}
-			//assign the digits after multi
-			while (carry !=0) {
+			// assign the digits after multi
+			while (carry != 0) {
 				array[digits++] = (int) (carry % 10);
 				carry /= 10;
 			}
 		}
 		int[] result = new int[digits];
-		result=Arrays.copyOf(array, digits);
+		result = Arrays.copyOf(array, digits);
 		return result;
 	}
-	
+
 	public static void main(String[] args) {
 		NumberRelated n = new NumberRelated();
-//		System.out.println("here"+n.factorial(25));
-//		System.out.println(Arrays.toString(n.FactorialLargeNumber(25)));
-		System.out.println(n.generate(21).size());
+		// System.out.println("here"+n.factorial(25));
+		// System.out.println(Arrays.toString(n.FactorialLargeNumber(25)));
+		System.out.println(n.isPalindrome(""));
 	}
 
 }
